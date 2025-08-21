fb8d9f5a39adeb7a0f449c04c1242fcfd040561d
tanakornsss <omtanakorn2550@gmail.com>
2025-08-21 21:41:22 +0700

    Added a main UI page for Gemma testing

---
+++ README.md
@@ -0,0 +1,3 @@
+# CaloriSense
+[@tanakornsss](https://github.com/tanakornsss)
+
---
+++ app/src/main/java/dev/tanakornsss/calorisense/GemmaTestScreen.kt
@@ -0,0 +1,8 @@
+package dev.tanakornsss.calorisense
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GemmaTestScreen() {
+
+}
