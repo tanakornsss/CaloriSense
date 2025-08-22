b892cfa58236dd34c4ac66cfc46075d14f7e7ada
tanakornsss <omtanakorn2550@gmail.com>
2025-08-22 18:29:37 +0700

    Add CPP side function implementation

--- app/src/main/cpp/calorisense.cpp
+++ app/src/main/cpp/calorisense.cpp
@@ -1,23 +1,6 @@
 #include <jni.h>
 #include <string>
 
-// Write C++ code here.
-//
-// Do not forget to dynamically load the C++ library into your application.
-//
-// For instance,
-//
-// In MainActivity.java:
-//    static {
-//       System.loadLibrary("calorisense");
-//    }
-//
-// Or, in MainActivity.kt:
-//    companion object {
-//      init {
-//         System.loadLibrary("calorisense")
-//      }
-//    }
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_dev_tanakornsss_calorisense_GemmaTestScreenKt_fetchNameCpp(JNIEnv *env, jclass) {
