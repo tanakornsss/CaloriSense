dea16482a91fbb33423fd4e77a300cb9f2f31cbb
tanakornsss <omtanakorn2550@gmail.com>
2025-08-22 15:42:11 +0700

    Add testing text

--- app/src/main/java/dev/tanakornsss/calorisense/GemmaTestScreen.kt
+++ app/src/main/java/dev/tanakornsss/calorisense/GemmaTestScreen.kt
@@ -1,8 +1,21 @@
 package dev.tanakornsss.calorisense
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun GemmaTestScreen() {
-
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Text("This is a test text")
+        }
+    }
 }
