ad57e5aa56f37e7b7560cacd322d41140204a0fb
tanakornsss <omtanakorn2550@gmail.com>
2025-08-27 21:22:22 +0700

    Remove unused functions. Prototyping input and output handling

--- app/src/main/java/dev/tanakornsss/calorisense/MainActivity.kt
+++ app/src/main/java/dev/tanakornsss/calorisense/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import dev.tanakornsss.calorisense.ui.screen.GemmaTestScreen
 import dev.tanakornsss.calorisense.ui.theme.CaloriSenseTheme
 
 class MainActivity : ComponentActivity() {
