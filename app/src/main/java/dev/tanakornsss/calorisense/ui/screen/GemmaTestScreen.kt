40d65c515008f8e688048e6e5b7a78a845a31ac4
tanakornsss <omtanakorn2550@gmail.com>
2025-08-28 21:28:11 +0700

    Enhance GemmaTestScreen with input field and output display

--- app/src/main/java/dev/tanakornsss/calorisense/ui/screen/GemmaTestScreen.kt
+++ app/src/main/java/dev/tanakornsss/calorisense/ui/screen/GemmaTestScreen.kt
@@ -1,20 +1,48 @@
 package dev.tanakornsss.calorisense.ui.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices.PIXEL_9
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun GemmaTestScreen() {
+    var inputTokenText by remember { mutableStateOf("") }
+
     Scaffold { innerPadding ->
         Column(
             modifier = Modifier.fillMaxSize()
                 .padding(innerPadding)
+                .padding(horizontal = 20.dp)
         ) {
-
+            OutlinedTextField(
+                label = { Text("Input your prompt") },
+                value = inputTokenText,
+                onValueChange = { inputTokenText = it },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(28.dp))
+            Text("The output returns here")
         }
     }
+}
+
+@Composable
+@Preview(device = PIXEL_9, showSystemUi = true)
+fun GemmaTestScreenPreview() {
+    GemmaTestScreen()
 }
