9a2b332ecf14ec280fe42ec0410adab16c4228c5
tanakornsss <omtanakorn2550@gmail.com>
2025-09-04 14:49:25 +0700

    Implement basic mutex

---
+++ app/src/main/cpp/util/util_token_manager.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Tanakorn on 9/4/2025.
+//
+
+#include <mutex>
+#include <string>
+#include "util_token_manager.h"
+
+std::mutex TokenManager::textTokenMutex;
+std::string TokenManager::textToken;
+
+TokenManager& TokenManager::getInstance() {
+    static TokenManager instance;
+    return instance;
+}
+
+void TokenManager::setTextToken(const std::string &token) {
+    std::lock_guard<std::mutex> lock(textTokenMutex);
+    textToken = token;
+}
+
+std::string TokenManager::getTextToken() {
+    std::lock_guard<std::mutex> lock(textTokenMutex);
+    return textToken;
+}
