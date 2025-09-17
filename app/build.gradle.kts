3bbb3cb988828444017093959df9a6f9d90bd7c2
tanakornsss <omtanakorn2550@gmail.com>
2025-09-17 21:21:29 +0700

    Add DataStore dependencies and update other dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
---
+++ app/src/main/java/dev/tanakornsss/calorisense/util/ModelPrefUtil.kt
@@ -0,0 +1,2 @@
+package dev.tanakornsss.calorisense.util
+
