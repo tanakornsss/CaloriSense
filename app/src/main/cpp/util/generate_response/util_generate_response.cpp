6ec7e0e63ffa9188ca52cb7ef038e306742f6249
tanakornsss <omtanakorn2550@gmail.com>
2025-09-25 11:55:47 +0700

    Identify the cause of crash

--- app/src/main/cpp/util/generate_response/util_generate_response.cpp
+++ app/src/main/cpp/util/generate_response/util_generate_response.cpp
@@ -52,15 +52,65 @@ std::string generate_response(const std::string& prompt, int maxTokens, llama_co
 
         // Tokenize
         std::vector<llama_token> tokens(prompt.size() + 32);
+        int n_input = llama_tokenize(
+                vocab,
+                prompt.c_str(),
+                static_cast<int32_t>(prompt.size()),
+                tokens.data(),
+                static_cast<int32_t>(tokens.size()),
+                true,
+                false
+        );
+        if (n_input < 0) {
+            LOG_E("n_input is below zero");
+            guard_unset();
+            return "";
+        }
+        LOG_I("n_input is %d", n_input);
 
+        LOG_I("tokens is initially %lu", tokens.size());
+        tokens.resize(n_input);
+        LOG_I("tokens is now %lu", tokens.size());
+
+        // Initialize batch
+        llama_batch batch = llama_batch_init(std::max(n_input, 1), 0, 1);
+        for (int i = 0; i < n_input; i++) {
+            batch.token[i] = tokens[i];
+            batch.pos[i] = i;
+            batch.n_seq_id[i] = 1;
+            batch.seq_id[i] = nullptr;
+            batch.logits[i] = (i == n_input - 1) ? 1 : 0;
+        }
+        batch.n_tokens = n_input;
+        LOG_I("batch.n_tokens is %d", batch.n_tokens);
+
+//        // Decode initial prompt ; crashed here
+//        if (llama_decode(ctx, batch) != 0) {
+//            LOG_E("Failed to decode initial prompt");
+//            llama_batch_free(batch);
+//            guard_unset();
+//            return "";
+//        }
+
+        // Sampling loop & generation
+        std::string res;
+        // Setup sampler chain (top-k, top-p, temp, etc.)
+        auto sparams = llama_sampler_chain_default_params();
+        llama_sampler *sampler = llama_sampler_chain_init(sparams);
+        llama_sampler_chain_add(sampler, llama_sampler_init_top_k(40));
+        llama_sampler_chain_add(sampler, llama_sampler_init_top_p(0.9f, 1));
+        llama_sampler_chain_add(sampler, llama_sampler_init_temp(0.8f));
+        llama_sampler_chain_add(sampler, llama_sampler_init_dist(12345));
+
+        guard_unset();
         return "completed";
     }
-    catch(const std::exception& e) {
+    catch (const std::exception &e) {
         LOG_E("generate_response: exception: %s", e.what());
         g_generating.store(false);
         return "";
     }
-    catch(...) {
+    catch (...) {
         LOG_E("generate_response: unknown exception");
         g_generating.store(false);
         return "";
