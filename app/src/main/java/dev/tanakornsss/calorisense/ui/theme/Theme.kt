5e6f5b148ee2d940a5b84c3c5d42fd9906ad1324
tanakornsss <omtanakorn2550@gmail.com>
2025-09-28 11:07:14 +0700

    Add new material expressive theme

--- app/src/main/java/dev/tanakornsss/calorisense/ui/theme/Theme.kt
+++ app/src/main/java/dev/tanakornsss/calorisense/ui/theme/Theme.kt
@@ -1,15 +1,19 @@
 package dev.tanakornsss.calorisense.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialExpressiveTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.expressiveLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -33,26 +37,26 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
-fun CaloriSenseTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+fun CaloriSenseTheme(content:  @Composable () -> Unit) {
+    val isDarkTheme = isSystemInDarkTheme()
+    val supportsDynamicColors = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colorScheme =
+        when {
+            supportsDynamicColors && isDarkTheme -> {
+                dynamicDarkColorScheme(LocalContext.current)
+            }
+            supportsDynamicColors && !isDarkTheme -> {
+                dynamicLightColorScheme(LocalContext.current)
+            }
+            isDarkTheme -> DarkColorScheme
+            !isDarkTheme -> LightColorScheme
+            else -> expressiveLightColorScheme()
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val shapes = Shapes(largeIncreased = RoundedCornerShape(36.0.dp))
+    MaterialExpressiveTheme(colorScheme = colorScheme, shapes = shapes) {
+        content()
     }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
 }
